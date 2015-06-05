29bbd43e2f0389382ff134b9273a6e858b12b3bc
ArendvD <arendvandormalen@gmail.com>
2015-06-05 12:08:03 +0200

    Bug fix + comments

--- VideoBlobDetectionSprint1.py
+++ VideoBlobDetectionSprint1.py
@@ -2,7 +2,7 @@
 import cv2
 import numpy as np;
 
-VIDEO_FILE = 'slaapkamerbrand2.mp4'
+VIDEO_FILE = 'aigfire.mp4'
 
 cap = cv2.VideoCapture(VIDEO_FILE);
 
@@ -13,6 +13,10 @@ while(cap.isOpened()):
 	# Read frame from video
 	RET, img = cap.read()
 
+	# Exit loop on last frame
+	if img == None:
+		break
+	
 	# define range of fire color
 	lower_fire = np.array([0,60,160])
 	upper_fire = np.array([80,170,255])
@@ -20,10 +24,11 @@ while(cap.isOpened()):
 	# Threshold the HSV image to get only fire colors
 	mask = cv2.inRange(img, lower_fire, upper_fire)
 
+	# Gaussian blur + Otsu's method to decrease noise (might not be useful)
 	blur = cv2.GaussianBlur(mask,(5,5),0)
 	ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-
+	# Erode and Dilate filters blobs
 	kernel = np.ones((5,5), np.uint8)
 	erosion = cv2.erode(th3,kernel,iterations=1)
 	dilation = cv2.dilate(erosion,kernel,iterations=1)
