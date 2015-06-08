a95c737baf1e6ff7bd40d283bf1a51bd68bcb5b7
Jochem <jochem@jchmb.nl>
2015-06-08 21:26:16 +0200

    Forgot command-line parameter

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import sys
 from Frame import Frame
 from Video import Video
 from FireMask import FireMask
 
-video = Video('aigfire.mp4')
+# check for command-line file, use default if none given
+if len(sys.argv) < 2:
+	VIDEO_FILE = 'aigfire.mp4'
+else:
+	VIDEO_FILE = sys.argv[1]
+
+video = Video(VIDEO_FILE)
 frame = Frame()
 frame.addTransformation(FireMask())
 frame.setVideo(video)
