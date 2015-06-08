da96dc81ee9aebe2f51d9f87dde4e5d1c47060f0
Jochem <jochem@jchmb.nl>
2015-06-08 15:41:31 +0200

    Added comments

--- Frame.py
+++ Frame.py
@@ -3,20 +3,36 @@ import time
 import numpy as np
 from Video import Video
 
+'''
+@class Frame
+
+Handles the windows and the video loop.
+'''
 class Frame:
 	def __init__(self):
 		cv2.namedWindow('Frame', cv2.WINDOW_NORMAL)
 		cv2.resizeWindow('Frame', 2133, 600)
 		self.video = None
 
+	'''
+	Set the video.
+
+	@param Video video
+	'''
 	def setVideo(self, video):
 		self.video = video
 
+	'''
+	Close the application, including the windows.
+	'''
 	def close(self):
 		if self.video != None:
 			self.video.close()
 		cv2.destroyAllWindows()
 
+	'''
+	Enter the video loop.
+	'''
 	def run(self):
 		ticks = int(time.time() * 1000)
 		while self.video != None:
@@ -26,6 +42,10 @@ class Frame:
 				if not self.step():
 					break
 
+	'''
+	Execute a single step. This involves reading a new image, as well as
+	checking for an exit event.
+	'''
 	def step(self):
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			self.close()
