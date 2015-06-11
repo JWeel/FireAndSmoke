95a9a09659899fd6946a4e7a512806f143e9bad0
Joseph <josefweel@gmail.com>
2015-06-11 14:32:16 +0200

    premerge

--- VideoFalseColor.py
+++ VideoFalseColor.py
@@ -3,31 +3,45 @@
 # Standard imports
 import cv2
 import numpy as np;
-
+import sys
+import time 
 #import os
 #for file in os.listdir("."):
 #    print(file)
 
-
-VIDEO_FILE = 'kelder_LWIR.mp4'
+if len(sys.argv) < 2:
+	VIDEO_FILE = 'aigfire.mp4'
+else:
+	VIDEO_FILE = sys.argv[1]
+	
+#VIDEO_FILE = 'kelder_LWIR.mp4'
 
 cap = cv2.VideoCapture(VIDEO_FILE);
-print cap
+#print cap
 
 if not cap.isOpened():
 	print 'error'
 
+# calculate frame-rate for updating bit-masking
+fps = cap.get(5) # 5 is index of the frame rate property of a video
+frametime = 1000 / fps
+
 while(cap.isOpened()):
-	print 'Open'
+	startlooptime = int(round(time.time() * 1000))
+	#print 'Open'
 	# Capture frame-by-frame
 	RET, frame = cap.read()
 
 	frame = cv2.applyColorMap(frame, 2);
-	frame = cv2.resize(frame, (0,0), fx = 4, fy = 4)
+	frame = cv2.resize(frame, (0,0), fx = 1, fy = 1)
 	cv2.imshow('frame_orig',frame)
 
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
+
+		# Run at 25 fps
+	while int(round(time.time() * 1000)) < startlooptime + frametime:
+		pass
 
 # When everything done, release the capture
 cap.release()
