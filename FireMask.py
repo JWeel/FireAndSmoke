c59fd0a1f7c4dc06e47e35c60bd220a8d66233f0
ArendvD <arendvandormalen@gmail.com>
2015-06-12 12:14:48 +0200

    Improved threshold

--- FireMask.py
+++ FireMask.py
@@ -4,20 +4,22 @@ import cv2
 
 class FireMask(Transformation):
 	def transform(self, img):
-		# define range of fire color
-		lower_fire = np.array([0,60,160])
-		upper_fire = np.array([80,170,255])
+		hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+		# get dimensions
+		height, width, depth = img.shape
+		redThres = 200
+		satThres = 150
 
-		# Threshold the HSV image to get only fire colors
-		mask = cv2.inRange(img, lower_fire, upper_fire)
+		mask = np.zeros((height, width), dtype=np.uint8)
 
-		# Gaussian blur + Otsu's method to decrease noise (might not be useful)
-		blur = cv2.GaussianBlur(mask,(5,5),0)
-		ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+		for i in range(0, height):
+			for j in range(0, width):
+				if img.item(i,j,2) > redThres and img.item(i,j,2) > img.item(i,j,1) and img.item(i,j,1) > img.item(i,j,0): 
+					if hsv.item(i,j,1) > 200: 
+						mask[i,j] = 255
 
-		# Erode and Dilate filters blobs
-		kernel = np.ones((5,5), np.uint8)
-		erosion = cv2.erode(th3,kernel,iterations=1)
+		kernel = np.ones((1,1), np.uint8)
+		erosion = cv2.erode(mask,kernel,iterations=1)
 		dilation = cv2.dilate(erosion,kernel,iterations=1)
 
 		# Bitwise-AND mask and original image
